c60bfb498e5521a1e50b917ce7d0e466d02f36e4
Illbeblack <shvartsdenis@mail.ru>
2024-01-04 14:26:43 +0200

    refactor: clean code

--- reactCV/src/components/StartSreen/StartScreen.jsx
+++ reactCV/src/components/StartSreen/StartScreen.jsx
@@ -11,38 +11,6 @@ function StartScreen() {
   });
   return (
     <div>
-      {/* <section className={styles.layers}>
-        <div className={styles.layers__container}>
-          <div className={`${styles.layers__item} ${styles.item1}`}></div>
-          <div className={`${styles.layers__item} ${styles.item2}`}></div>
-          <div className={`${styles.layers__item} ${styles.item3}`}></div>
-          <div className={`${styles.layers__item} ${styles.item4}`}>
-            <div className={styles.content}>
-              <h1>Shvarts Denis</h1>
-              <h4>Front-end Developer</h4>
-              <div className={styles.buttonContainer}>
-                <a
-                  href="https://www.linkedin.com/in/denis-shvarts-051081275/"
-                  className={styles.LinkedIn}
-                >
-                  <img
-                    src="../../../public/linkedin-96.svg"
-                    alt="link to LinkedIn"
-                  />
-                </a>
-                <button className={styles.startButton}>move to CV</button>
-                <a
-                  href="mailto:ShvartsDenisA@gmail.com"
-                  className={styles.gmail}
-                >
-                  <img src="../../../public/gmail-96.svg" alt="link to Gmail" />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className={`${styles.layers__item} ${styles.item5}`}></div>
-        </div>
-      </section> */}
       <section className={styles.layers}>
         <div className={styles.layers__container}>
           <div className={`${styles.layers__item} ${styles.item1}`}></div>
@@ -56,6 +24,7 @@ function StartScreen() {
               <div className={styles.buttonContainer}>
                 <a
                   href="https://www.linkedin.com/in/denis-shvarts-051081275/"
+                  target="blank"
                   className={styles.LinkedIn}
                 >
                   <img
