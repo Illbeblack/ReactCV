a3a662faf8eb1b010dc7e3644a25e77166505772
Illbeblack <shvartsdenis@mail.ru>
2024-01-04 14:21:27 +0200

    feat: make redesign of start screen

--- reactCV/src/components/StartSreen/StartScreen.jsx
+++ reactCV/src/components/StartSreen/StartScreen.jsx
@@ -11,14 +11,7 @@ function StartScreen() {
   });
   return (
     <div>
-      <div className={styles.logoContainer}>
-        <img
-          src="../../../public/ico.png"
-          alt="logo"
-          className={styles.logo}
-        ></img>
-      </div>
-      <section className={styles.layers}>
+      {/* <section className={styles.layers}>
         <div className={styles.layers__container}>
           <div className={`${styles.layers__item} ${styles.item1}`}></div>
           <div className={`${styles.layers__item} ${styles.item2}`}></div>
@@ -48,6 +41,38 @@ function StartScreen() {
             </div>
           </div>
           <div className={`${styles.layers__item} ${styles.item5}`}></div>
+        </div>
+      </section> */}
+      <section className={styles.layers}>
+        <div className={styles.layers__container}>
+          <div className={`${styles.layers__item} ${styles.item1}`}></div>
+          <div className={`${styles.layers__item} ${styles.item2}`}></div>
+          <div className={`${styles.layers__item} ${styles.item3}`}></div>
+          <div className={`${styles.layers__item} ${styles.item4}`}></div>
+          <div className={`${styles.layers__item} ${styles.item5}`}>
+            <div className={styles.content}>
+              <h1>Shvarts Denis</h1>
+              <h4>Front-end Developer</h4>
+              <div className={styles.buttonContainer}>
+                <a
+                  href="https://www.linkedin.com/in/denis-shvarts-051081275/"
+                  className={styles.LinkedIn}
+                >
+                  <img
+                    src="../../../public/linkedin-96.svg"
+                    alt="link to LinkedIn"
+                  />
+                </a>
+                <button className={styles.startButton}>move to CV</button>
+                <a
+                  href="mailto:ShvartsDenisA@gmail.com"
+                  className={styles.gmail}
+                >
+                  <img src="../../../public/gmail-96.svg" alt="link to Gmail" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
